594ad59a8851f7c8fc00820619006b3ef91be639
anacristinaneves2 <achristina.bezerra@gmail.com>
2024-09-01 12:16:12 -0300

    Add files via upload

---
+++ projeto-01-02/main.js
@@ -0,0 +1,13 @@
+console.log ("Meu nome é Ana Cristina")
+console.log("Eu tenho 24 anos")
+
+let firstName = "Ana"
+let lastName ="Neves"
+let age = 24
+
+
+console.log ('Meu nome é '+ firstName + ' ' + lastName)
+console.log('Eu tenho ' + age + ' anos de idade')
+
+
+
